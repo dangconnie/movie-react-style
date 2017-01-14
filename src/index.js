eeab2bd7fd4756e7c19ac196e9d5847f12a7b3cf
Connie Dang <conniedang576@gmail.com>
2017-01-13 22:58:12 -0500

    init commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+//Import: go find __ in this file and call it ____.
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import SingleMovie from './SingleMovie'
+
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
+ReactDOM.render(
+ 
+	<Router history={hashHistory}>
+		<Route path="/" component={App} />
+		<Route path="/:movie" component={SingleMovie} />
+		<Route path="/:movie/:cast" component={SingleMovie} />
+
+	</Router>,
+  document.getElementById('root')
+);
+
+
